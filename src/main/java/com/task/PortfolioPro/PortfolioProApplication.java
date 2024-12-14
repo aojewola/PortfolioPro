1f22f39f622a918c4bc6671a89b06ea4d14f75cf
Asimi Ojewola <asimi.ojewola@gmail.com>
2024-12-14 23:08:44 +0000

    Spring boot project setup for PortfolioPro

---
+++ src/main/java/com/task/PortfolioPro/PortfolioProApplication.java
@@ -0,0 +1,13 @@
+package com.task.PortfolioPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/PortfolioPro/PortfolioProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.PortfolioPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
